b145f5e0e6c1c69f7cfb46cd39182e4da9a1a6d2
Yugandharkumar <yugaa22@gmail.com>
2023-02-09 17:51:19 +0530

    Update settings.js

--- halconfig/settings.js
+++ halconfig/settings.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var uiHost = '{%ui.baseUrl%}';
 var gateHost = '{%gate.baseUrl%}';
 var atlasWebComponentsUrl = '{%canary.atlasWebComponentsUrl%}';
 var authEnabled = '{%features.auth%}' === 'true';
