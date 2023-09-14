e043909501b4ec58a317d5eae20d6cf4ff150362
Rajinderpal Singh Siddhu <61963157+siddhu-opsmx@users.noreply.github.com>
2023-09-14 13:20:00 +0530

    ECS changes related to Disable server group tab (#79)

--- packages/core/src/config/settings.ts
+++ packages/core/src/config/settings.ts
@@ -151,6 +151,7 @@ export interface ISpinnakerSettings {
   kubernetesAdHocInfraWritesEnabled: boolean;
   kubernetesAdHocInfraEditEnabled: boolean;
   changelogUrl: string;
+  ecsAdHocInfraEditEnabled: boolean;
 }
 
 export const SETTINGS: ISpinnakerSettings = (window as any).spinnakerSettings || {};
@@ -160,6 +161,7 @@ SETTINGS.feature = SETTINGS.feature || {};
 SETTINGS.feature.roscoMode = SETTINGS.feature.roscoMode ?? true;
 SETTINGS.kubernetesAdHocInfraWritesEnabled = SETTINGS.kubernetesAdHocInfraWritesEnabled ?? true;
 SETTINGS.kubernetesAdHocInfraEditEnabled = SETTINGS.kubernetesAdHocInfraEditEnabled ?? false;
+SETTINGS.ecsAdHocInfraEditEnabled = SETTINGS.ecsAdHocInfraEditEnabled ?? false;
 SETTINGS.analytics = SETTINGS.analytics || {};
 SETTINGS.providers = SETTINGS.providers || {};
 SETTINGS.defaultTimeZone = SETTINGS.defaultTimeZone || 'America/Los_Angeles';
